c8ba04a79639f8c30734bbbedca7e2056673047f
akudewaganteng <yvinn435@gmail.com>
2025-06-10 11:57:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,27 +2,19 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const apiKey = "apikey123"; // Ganti sesuai kebutuhan
+const apiKey = "appolodb";
 
-// Serve static file (index.html)
 app.use(express.static(__dirname));
 
-// Cek API key via GET
+app.get("/check", (req, res) => {
+  const inputKey = req.query.apikey;
+  const isValid = inputKey === apiKey;
+
+  res.json({ valid: isValid });
+});
+
 app.get("/", (req, res) => {
-  const key = req.query.apikey;
-
-  if (!key) {
-    // Jika tidak ada query, kirim index.html
-    return res.sendFile(path.join(__dirname, "index.html"));
-  }
-
-  if (key === apiKey) {
-    // API key benar
-    res.send(`<script>alert("API key benar"); window.location.href="/";</script>`);
-  } else {
-    // API key salah
-    res.send(`<script>alert("API key salah"); window.location.href="/";</script>`);
-  }
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 module.exports = app;
